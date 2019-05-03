11f6af78b6d9e274a89e8d070f18d674a2c4b10b
lmarchan <49087099+lmarchan@users.noreply.github.com>
2019-05-03 18:10:13 -0500

    Discretizing u(x,y)

--- src/SC_Project.m
+++ src/SC_Project.m
@@ -23,40 +23,54 @@ ny=20;
 hx = (bx-ax)/(nx-1);
 hy = (by-ay)/(ny-1);
 
-% Boudary conditions for du/dx(x=ax)=0 & du/dx(x=bx)=0 
-
-% Outlining the function F(x,y) 
-%F(x,y) = sin(pi*(x-ax)/(bx-ax))*cos(pi*(2*(y-ay)/(by-ay)+1)/2);
-
 % Ghost nodes
 axneg=ax-hx;
-ayneg=ay-hy;
+% ayneg=ay-hy; This makes ghost nodes on bottom boundary (not needed)
 nxplus=nx+2;
-nyplus=ny+2;
-
-% Discretizing x and y, and boundary conditions for u(x,ay),
-% u(x,by),(du/dx)@ax = 0 & (du/dx)@bx = 0
+nyplus=ny+2; %This makes ghost nodes on top boundary (not needed)
 
 %preallocation
 x=zeros(1,nxplus);
 phi=zeros(1,nxplus);
 psi=zeros(1,nxplus);
-y=zeros(1,nyplus);
-rho=zeros(1,nyplus);
-theta=zeros(1,nyplus);
+y=zeros(1,ny);               %       y=zeros(1,nyplus);    (old)
+                             %       rho=zeros(1,nyplus);
+                             %       theta=zeros(1,nyplus);
 
-for j = 1:nxplus
-    x(j) = axneg+hx*(j-1);
-    phi(j) = (x(j)-ax)*(x(j)-ax)*sin(pi*(x(j)-ax)/(2*(bx-ax)));
-    psi(j) = (cos(pi*(x(j)-ax))-1)*cosh(bx-x(j));
+% Discretizing x, and boundary conditions for u(x,ay) and u(x,by),
+
+
+for k = 1:nxplus
+    x(k) = axneg+hx*(k-1);
+    phi(k) = (x(k)-ax)*(x(k)-ax)*sin(pi*(x(k)-ax)/(2*(bx-ax))); %Boundary Conditions @y=-pi
+    psi(k) = (cos(pi*(x(k)-ax))-1)*cosh(bx-x(k));   %Boundary Conditions @y=pi
+end
+% Discretizing y
+% u(x,by),(du/dx)@ax = 0 & (du/dx)@bx = 0
+for j = 1:ny                %      k = 1:nyplus    (old)
+    y(j) = ay+hy*(j-1);     %       y(k) = ayneg+hy*(k-1);   (old)   
+                            %     rho(k) = phi(2);      (incorrect)
+                            %     theta(k) = psi(nyplus-1);  (incorrect)
 end
 
-for k = 1:nyplus
-    y(k) = ayneg+hy*(k-1);
-    rho(k) = phi(2);
-    theta(k) = psi(nyplus-1);
+% Discretizing the function F(x,y) **
+F=zeros(ny,nxplus);
+u=zeros(ny,nxplus);
+for j = 1:ny
+    for k = 1:nxplus       
+        F(j,k) = sin(pi*(x(k)-ax)/(bx-ax)).*cos(pi*(2*(y(j)-ay)/(by-ay)+1)/2);
+    end
 end
-
-    
-
+u(1,:) = psi;
+u(ny,:) = phi;
+u(:,1) = u(:,3);
+u(:,nxplus) = u(:,nxplus-2);
+for i = 1:100
+    for j = 2:ny-1 
+        u(:,1) = u(:,3);
+        for k = 2:nxplus-1       
+            u(j,k) = u(j,k-1)+u(j,k+1)+u(j-1,k)+u(j+1,k)-4*u(j,k)+F(j,k)*hy*hx;
+        end
+    end
+end
 
