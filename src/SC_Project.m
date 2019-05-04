1613872af035a6115cc3e318c3067e506d672f3b
lmarchan <49087099+lmarchan@users.noreply.github.com>
2019-05-04 04:42:37 -0500

    Jacobi Method Step

--- src/SC_Project.m
+++ src/SC_Project.m
@@ -25,52 +25,61 @@ hy = (by-ay)/(ny-1);
 
 % Ghost nodes
 axneg=ax-hx;
-% ayneg=ay-hy; This makes ghost nodes on bottom boundary (not needed)
 nxplus=nx+2;
-nyplus=ny+2; %This makes ghost nodes on top boundary (not needed)
+nyplus=ny+2; 
 
-%preallocation
+% Preallocation
 x=zeros(1,nxplus);
 phi=zeros(1,nxplus);
 psi=zeros(1,nxplus);
-y=zeros(1,ny);               %       y=zeros(1,nyplus);    (old)
-                             %       rho=zeros(1,nyplus);
-                             %       theta=zeros(1,nyplus);
+y=zeros(1,ny);               
 
 % Discretizing x, and boundary conditions for u(x,ay) and u(x,by),
-
-
-for k = 1:nxplus
+for k = 2:nxplus-1
     x(k) = axneg+hx*(k-1);
     phi(k) = (x(k)-ax)*(x(k)-ax)*sin(pi*(x(k)-ax)/(2*(bx-ax))); %Boundary Conditions @y=-pi
     psi(k) = (cos(pi*(x(k)-ax))-1)*cosh(bx-x(k));   %Boundary Conditions @y=pi
 end
 % Discretizing y
 % u(x,by),(du/dx)@ax = 0 & (du/dx)@bx = 0
-for j = 1:ny                %      k = 1:nyplus    (old)
-    y(j) = ay+hy*(j-1);     %       y(k) = ayneg+hy*(k-1);   (old)   
-                            %     rho(k) = phi(2);      (incorrect)
-                            %     theta(k) = psi(nyplus-1);  (incorrect)
+for j = 1:ny               
+    y(j) = ay+hy*(j-1);    
 end
 
-% Discretizing the function F(x,y) **
+% Preallocation
 F=zeros(ny,nxplus);
 u=zeros(ny,nxplus);
-for j = 1:ny
-    for k = 1:nxplus       
-        F(j,k) = sin(pi*(x(k)-ax)/(bx-ax)).*cos(pi*(2*(y(j)-ay)/(by-ay)+1)/2);
-    end
-end
+
+% Setting boundary conditions in place
 u(1,:) = psi;
 u(ny,:) = phi;
-u(:,1) = u(:,3);
-u(:,nxplus) = u(:,nxplus-2);
-for i = 1:100
-    for j = 2:ny-1 
-        u(:,1) = u(:,3);
-        for k = 2:nxplus-1       
-            u(j,k) = u(j,k-1)+u(j,k+1)+u(j-1,k)+u(j+1,k)-4*u(j,k)+F(j,k)*hy*hx;
-        end
-    end
-end
 
+tic
+for i = 1:100
+    
+    for j = 2:ny-1 
+        %F(j,2) =
+        %sin(pi*(x(2)-ax)/(bx-ax)).*cos(pi*(2*(y(j)-ay)/(by-ay)+1)/2);
+        %(old)
+        %u(j,2) = (2*u(j,3)+u(j-1,2)+u(j+1,2)+F(j,2)*hy*hx)/4;
+        %F(j,nxplus-1) =
+        %sin(pi*(x(nxplus-1)-ax)/(bx-ax)).*cos(pi*(2*(y(j)-ay)/(by-ay)+1)/2);
+        %(old)
+        %u(j,nxplus-1) = (2*u(j,nxplus-2)+u(j-1,nxplus-1)+u(j+1,nxplus-1)+F(j,nxplus-1)*hy*hx)/4;
+        u(:,1) = u(:,3);
+        u(:,nxplus) = u(:,nxplus-2);
+        for k = 2:nxplus-1      
+            F(j,k) = sin(pi*(x(k)-ax)/(bx-ax)).*cos(pi*(2*(y(j)-ay)/(by-ay)+1)/2);
+            u(j,k) = (u(j,k-1)+u(j,k+1)+u(j-1,k)+u(j+1,k)+F(j,k)*hy*hx)/4;
+        end
+        
+        
+    end
+    i
+    graph = surf(x,y,u);
+    drawnow;
+    
+    refreshdata(graph)
+end
+    
+toc
